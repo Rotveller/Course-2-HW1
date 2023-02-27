dda85ccd5645c584b732ce0ab63473e3a8d2d2ac
Rotveller <bulatovalexsandr1999@gmail.com>
2023-02-27 19:27:56 +0300

    Step 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PersonService personService = new PersonService();
+        Person sarah = new Person("Sarah", 30);
+        if (sarah.child != null) {
+            System.out.println("sarah.child.getName() = " + sarah.child.getName());
+        } else {
+            System.out.println("No child");
+        }
+        Person john = new Person("John", 13);
+        sarah.child = john;
+
+        personService.SetAgeToEighTeen(sarah);
+        if (sarah.child != null) {
+            System.out.println("sarah.child.getName().length() = " + sarah.child.getName().length());
+        }
+        System.out.println("john.getName() = " + john.getName());
+        System.out.println("sarah.child.getName() = " + sarah.child.getName());
+        System.out.println("sarah.getAge() = " + sarah.getAge());
+        john = new Person("Alternative John", 30);
+        john = null;
+
     }
 }
